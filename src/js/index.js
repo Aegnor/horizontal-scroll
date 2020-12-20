3a4b3147af08c90422655188fc63b61abe2bb577
Sbr <12alexman@gmail.com>
2020-12-20 19:06:45 +0200

    refactor: added options, make some vars - readonly, added destroy event and much more

--- src/js/index.js
+++ src/js/index.js
@@ -5,90 +5,148 @@ import {getMousePosition} from './utils/mouse.js'
 import NormalizeWheel from './utils/normalizeWheel.js'
 
 class Scroll {
-    constructor(selector) {
+    constructor(selector, options) {
         this.element = typeof selector === 'string' ?  document.querySelector(selector) : selector
+        this.maxScroll = this.element.clientWidth - window.innerWidth
         this.scroll = {
-            target: 0,
-            current: 0,
-            off: 0,
-            speed: 1.5,
-            drag: false
+            _scrollValue: 0,
+            _latestScrollValue: 0,
+            _drag: false,
+            dragSpeed: options.dragSpeed,
+            wheelScrollValue: options.wheelScrollValue,
+            keyBoardScrollValue: options.keyBoardScrollValue,
+            keyBoardScrollValueIncrementer: options.keyBoardScrollValueIncrementer
         }
+        this.onWheelCb = this.onWheel.bind(this)
+        this.onScrollTouchStartCb = this.onScrollTouchStart.bind(this)
+        this.onScrollTouchMoveCb = this.onScrollTouchMove.bind(this)
+        this.onScrollTouchEndCb = this.onScrollTouchEnd.bind(this)
+        this.onScrollKeyboardDownCb = this.onScrollKeyboardDown.bind(this)
+        this.onScrollKeyboardUpCb = this.onScrollKeyboardUp.bind(this)
     }
 
     onWheel(event) {
-        const
-            wheel = new NormalizeWheel(event),
-            delta = wheel.spinY * 50
+        const wheel = new NormalizeWheel(event)
 
-        this.scroll.off += delta * this.scroll.speed
-        this.scroll.target = this.scroll.off
+        this.scroll._latestScrollValue += wheel.spinY * this.scroll.wheelScrollValue
+
+        this._checkIfFitsWindow()
+
+        this.scroll._scrollValue = this.scroll._latestScrollValue
+
+        this.onScrollUpdate()
     }
 
     onScrollTouchStart(event) {
-        this.scroll.drag = true;
-        event.preventDefault();
+        event.preventDefault()
+        this.scroll._drag = true
+
+        this.element.classList.add('is-grabbing')
 
         this.startX = getMousePosition(event).x
     }
 
-    onScrollTouchEnd() {
-        this.scroll.drag = false;
-        this.scroll.off = this.scroll.target
-    }
-
     onScrollTouchMove(event) {
-        if (!this.scroll.drag) return
+        if (!this.scroll._drag) return
 
-        const {x} = getMousePosition(event)
-        this.distance = (x - this.startX) * -1
+        this.distance = (getMousePosition(event).x - this.startX) * -1
 
-        this.scroll.target = this.scroll.off + this.distance * this.scroll.speed
-    }
-
-    onScrollKeyboard(event) {
-        const keyCode = event.code
-
-        if (keyCode === "ArrowRight") {
-            this.scroll.off += 20
-            this.scroll.target = this.scroll.off
-        } else if (keyCode === "ArrowLeft") {
-            this.scroll.off -= 20
-            this.scroll.target = this.scroll.off
-        }
-    }
-
-    onScrollUpdate() {
-        const total = this.element.clientWidth - window.innerWidth
-
-        if (this.scroll.target < 0) this.scroll.target = 0
-        if (this.scroll.target > total) this.scroll.target = total
-
-        gsap.to(this.element, {
-            x: -this.scroll.target,
-            duration: 0.3,
-        })
-
-        window.requestAnimationFrame(this.onScrollUpdate.bind(this))
-    }
-
-    init() {
-        document.addEventListener('wheel', this.onWheel.bind(this))
-
-        document.addEventListener('mousedown', this.onScrollTouchStart.bind(this))
-        document.addEventListener('mousemove', this.onScrollTouchMove.bind(this))
-        document.addEventListener('mouseup', this.onScrollTouchEnd.bind(this))
-
-        document.addEventListener('touchstart', this.onScrollTouchStart.bind(this), { passive: false })
-        document.addEventListener('touchmove', this.onScrollTouchMove.bind(this))
-        document.addEventListener('touchend', this.onScrollTouchEnd.bind(this))
-
-        document.addEventListener('keydown', this.onScrollKeyboard.bind(this))
+        this.scroll._scrollValue = this.scroll._latestScrollValue + this.distance * this.scroll.dragSpeed
 
         this.onScrollUpdate()
     }
+
+    onScrollTouchEnd() {
+        this.scroll._drag = false
+        this.element.classList.remove('is-grabbing')
+        this.scroll._latestScrollValue = this.scroll._scrollValue
+    }
+
+    onScrollKeyboardDown(event) {
+        const
+            keyCode = event.code,
+            // TODO: make speed increase by multiply not plus
+            scrolledFromKeyBoard = this.scroll.keyBoardScrollValue + this.scroll.keyBoardScrollValueIncrementer++
+
+        if (keyCode === "ArrowRight") {
+            this.scroll._latestScrollValue += scrolledFromKeyBoard
+        }
+
+        if (keyCode === "ArrowLeft") {
+            this.scroll._latestScrollValue -= scrolledFromKeyBoard
+        }
+
+        this._checkIfFitsWindow()
+
+        this.scroll._scrollValue = this.scroll._latestScrollValue
+
+        this.onScrollUpdate()
+    }
+
+    _checkIfFitsWindow() {
+        if (this.scroll._latestScrollValue <= 0) this.scroll._latestScrollValue = 0
+
+        if (this.scroll._latestScrollValue >= this.maxScroll) this.scroll._latestScrollValue = this.maxScroll
+    }
+
+    onScrollKeyboardUp() {
+        this.scroll.keyBoardScrollValueIncrementer = this.scroll.keyBoardScrollValue
+    }
+
+    onScrollUpdate() {
+        if (this.scroll._scrollValue < 0) this.scroll._scrollValue = 0
+        if (this.scroll._scrollValue > this.maxScroll) this.scroll._scrollValue = this.maxScroll
+
+        gsap.to(this.element, {
+            x: -this.scroll._scrollValue,
+            duration: 0.3,
+        })
+    }
+
+    init() {
+        document.addEventListener('wheel', this.onWheelCb)
+
+        document.addEventListener('mousedown', this.onScrollTouchStartCb)
+        document.addEventListener('mousemove', this.onScrollTouchMoveCb)
+        document.addEventListener('mouseup', this.onScrollTouchEndCb)
+
+        document.addEventListener('touchstart', this.onScrollTouchStartCb, { passive: false })
+        document.addEventListener('touchmove', this.onScrollTouchMoveCb)
+        document.addEventListener('touchend', this.onScrollTouchEndCb)
+
+        document.addEventListener('keydown', this.onScrollKeyboardDownCb)
+        document.addEventListener('keyup', this.onScrollKeyboardUpCb)
+    }
+
+    destroy() {
+        document.removeEventListener('wheel', this.onWheelCb)
+
+        document.removeEventListener('mousedown', this.onScrollTouchStartCb)
+        document.removeEventListener('mousemove', this.onScrollTouchMoveCb)
+        document.removeEventListener('mouseup', this.onScrollTouchEndCb)
+
+        document.removeEventListener('touchstart', this.onScrollTouchStartCb)
+        document.removeEventListener('touchmove', this.onScrollTouchMoveCb)
+        document.removeEventListener('touchend', this.onScrollTouchEndCb)
+
+        document.removeEventListener('keydown', this.onScrollKeyboardDownCb)
+        document.removeEventListener('keyup', this.onScrollKeyboardUpCb)
+    }
 }
 
-const scroll = new Scroll('.scroll')
+const scroll = new Scroll('.scroll', {
+    dragSpeed: 2,
+    wheelScrollValue: 50,
+    keyBoardScrollValue: 20,
+    keyBoardScrollValueIncrementer: 120
+})
 
 scroll.init()
+
+document.getElementById('destroy').addEventListener('click', function () {
+    scroll.destroy()
+})
+
+document.getElementById('init').addEventListener('click', function () {
+    scroll.init()
+})
